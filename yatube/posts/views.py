940f94c995a0f4217b86ecf184344ad0b6bceca1
Zaveryukhin Igor <zaveryukhinigor.com>
2024-05-25 22:44:45 +1000

    HTML pushing

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Post
+
 
 def index(request):
-    text = "Это главная страница проекта Yatube"
-    context = {"text": text}
-    template = 'posts/index.html'
-    return render(request, template, context)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
